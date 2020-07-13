b0259e641d2c7121b333bfd5cc84706fe2db1494
Tom Arra <tarra3@gmail.com>
2020-07-13 17:58:52 -0500

    Initial commit

---
+++ install.ps1
@@ -0,0 +1,11 @@
+# Development
+
+winget install --id Microsoft.VisualStudioCode -e
+winget install --id Git.Git -e
+
+# Games
+
+# Other
+
+winget install --id Spotify.Spotify -e
+winget install --id SlackTechnologies.Slack -e
