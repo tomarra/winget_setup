be8fbdfc1c1d6f6c12514efc0ff69bd759cbada7
Tom Arra <tarra3@gmail.com>
2020-07-13 18:03:02 -0500

    Add files via upload

--- install.ps1
+++ install.ps1
@@ -1,7 +1,12 @@
+# Basics
+
+winget install --id Microsoft.Edge -e
+
 # Development
 
 winget install --id Microsoft.VisualStudioCode -e
 winget install --id Git.Git -e
+winget install --id Microsoft.WindowsTerminal -e
 
 # Games
 
